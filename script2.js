f0a3ee9163539fdd9a7f9aeed75f2c0e3ef061f2
Raymond <cancinoray@gmail.com>
2022-06-17 15:06:43 +0800

    updating files

--- script2.js
+++ script2.js
@@ -11,29 +11,18 @@ if(birthDate){
 }
 
 const fbirthDate = birthDate.split("-").reverse().join("/");
-// const padTo2Digits = (num) => {
-//   return num.toString().padStart(2, '0');
-// }
-
-// const formatDate = (date) => {
-//   return [
-//     padTo2Digits(date.getDate()),
-//     padTo2Digits(date.getMonth() + 1),
-//     date.getFullYear(),
-//   ].join('/');
-// }
 
      //collect input from HTML form and convert into date format  
      //birthDate from localStorage and new Date() function
 
     //calculate the difference of dates  
-    const diffInMillisecond = new Date() - new Date(birthDate);
-    // console.log('diffInMillisecond', diffInMillisecond)
+const diffInMillisecond = new Date() - new Date(birthDate);
+// console.log('diffInMillisecond', diffInMillisecond)
 
-     //convert the difference in milliseconds and store in day and year variable 
-    const year_age = Math.floor(diffInMillisecond / 31536000000);  
-    const day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);
-    const month_age = Math.floor(day_age/30); 
+ //convert the difference in milliseconds and store in day and year variable 
+const year_age = Math.floor(diffInMillisecond / 31536000000);  
+const day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);
+const month_age = Math.floor(day_age/30); 
 
     // console.log('year_age' , year_age );
     // console.log('day_age' , day_age )
@@ -44,7 +33,7 @@ const fbirthDate = birthDate.split("-").reverse().join("/");
 
 document.getElementById('rtoday').innerText = today;
 document.getElementById('rname').innerText = name;
-document.getElementById('remail').innerHTML =  email
+document.getElementById('remail').innerHTML =  email;
 document.getElementById('rbirthDate').innerText = fbirthDate;
 document.getElementById('rname').innerText = name;
 document.getElementById('rcolor').innerText = color;
